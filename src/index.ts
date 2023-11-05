3432c8f636dff515f63e289ec1d6f261a54cb8e5
RubensLFerreira <rubenslima000@gmail.com>
2023-11-05 00:51:11 -0300

    clear all

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { server } from './server/Server';
-
-server.listen(process.env.PORT || 8080, () => {
-  console.log(`Server running at port http://localhost:${process.env.PORT}/`);
-});
--- src/server/controllers/cliente/index.ts
+++
@@ -1,5 +0,0 @@
-import * as getAllController from './GetAllController';
-
-export const ClienteController = {
-  ...getAllController,
-};
