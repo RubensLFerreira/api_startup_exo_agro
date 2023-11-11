810d10923204c101b38c8cbc89533258c92db53a
RubensLFerreira <rubenslima000@gmail.com>
2023-11-11 12:03:18 -0300

    chore: Adding routes of customers

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 
 import { StatusCodes } from 'http-status-codes';
 
-import clienteGetAll from '../controllers/cliente/GetAll.js';
+import getAllCustomers from '../controllers/cliente/GetAll.js';
+import createCustomer from '../controllers/cliente/Create.js';
+import updateCustomer from '../controllers/cliente/UpdateById.js';
+import deleteCustomer from '../controllers/cliente/DeleteById.js';
 
 const router = express.Router();
 
@@ -10,6 +13,9 @@ router.get('/', (_, res) => {
   return res.status(StatusCodes.ACCEPTED).send('Hello world! Página inicial');
 });
 
-router.get('/clientes', clienteGetAll.getAllClientes);
+router.get('/clientes', getAllCustomers.getAll);
+router.post('/cliente/cadastrar', createCustomer.create);
+router.put('/cliente/editar/:id', updateCustomer.updateById);
+router.delete('/cliente/deletar/:id', deleteCustomer.deleteById);
 
 export default router;
