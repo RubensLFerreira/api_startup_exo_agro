59d8d5554c9ebc9d67036b729ae1f19901e3d0cb
RubensLFerreira <rubenslima000@gmail.com>
2023-11-12 21:20:23 -0300

    feat: function responsible for image upload

---
+++ src/server/middlewares/uploadImage.js
@@ -0,0 +1,45 @@
+import multer from 'multer';
+import path from 'path';
+
+const imageStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const { type } = req.params;
+    let folder = 'cliente';
+
+    switch (type) {
+      case 'visita':
+        folder = 'visita';
+        break;
+      case 'admin':
+        folder = 'folder';
+        break;
+      default:
+        break;
+    }
+
+    const uploadPath = `src/server/public/images/${folder}`;
+    callback(null, uploadPath);
+  },
+
+  filename: (req, file, callback) => {
+    const extension = path.extname(file.originalname);
+    const fileName =
+      Date.now() + String(Math.floor(Math.random() * 999)) + extension;
+    callback(null, fileName);
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+
+  fileFilter(req, file, callback) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+      return callback(
+        new Error('Please only send images in png, jpeg or jpg format!'),
+      );
+    }
+    callback(null, true);
+  },
+});
+
+export default imageUpload;
