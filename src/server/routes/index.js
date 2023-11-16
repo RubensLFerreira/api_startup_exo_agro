c906869eb52306251584409bfa6de3aa2b52f141
RubensLFerreira <rubenslima000@gmail.com>
2023-11-16 00:04:52 -0300

    feat: adding new routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -13,13 +13,20 @@ import createCustomer from '../controllers/client/Create.js';
 import updateCustomer from '../controllers/client/UpdateById.js';
 import deleteCustomer from '../controllers/client/DeleteById.js';
 
+import getAllAgronomist from '../controllers/agronomo/GetAll.js';
+import createAgronomist from '../controllers/agronomo/Create.js';
+
 const router = express.Router();
 
 router.get('/', (_, res) => {
   return res.status(StatusCodes.ACCEPTED).send('Hello world! Página inicial');
 });
 
-router.post('/admin/cadastrar/:tipo', imageUpload.single('foto'), createAdmin.create);
+router.post(
+  '/admin/cadastrar/:tipo',
+  imageUpload.single('foto'),
+  createAdmin.create,
+);
 
 router.get('/clientes', authenticated, getAllCustomers.getAll);
 router.get('/cliente/:id', getCustomerById.getById);
@@ -30,5 +37,12 @@ router.post(
 );
 router.put('/cliente/editar/:id', updateCustomer.updateById);
 router.delete('/cliente/deletar/:id', deleteCustomer.deleteById);
+
+router.post(
+  '/agronomo/cadastrar/:tipo',
+  imageUpload.single('foto'),
+  createAgronomist.create,
+);
+router.get('/agronomos', getAllAgronomist.getAll);
 
 export default router;
